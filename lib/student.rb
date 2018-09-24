4ab0c1760942e3bfddb2cd36af05ef7768aa8935
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-24 01:49:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,8 +18,9 @@ class Student
       FROM students
     SQL
 
-    DB[:conn].execute(sql)
-    self.new_from_db(row)
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
   def self.find_by_name(name)
@@ -77,6 +78,8 @@ class Student
     DB[:conn].execute(sql).collect do |row|
      self.new_from_db(row)
    end
+
+   
   end
 
 end
