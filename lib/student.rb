460f48bc4f4f228ba78d3284a86e9e8e7d810e72
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-09-24 01:43:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,7 +71,10 @@ class Student
   end
 
   def self.students_below_12th_grade
-    
+    sql = <<-SQL
+    SELECT name FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
